9c1efad9cb3386c88b77242db6f648c30dc62e75
Sisamgtm07 <sisamgautam0001@gmail.com>
2020-06-10 14:38:32 +0545

    Menu-toggle added

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function myfunction(){
+    let hamelement=document.getElementById("nav-list");
+    if(hamelement.style.display==="none"){
+        hamelement.style.display="block";
+    }
+    else{
+        hamelement.style.display="none";
+    }
+}
